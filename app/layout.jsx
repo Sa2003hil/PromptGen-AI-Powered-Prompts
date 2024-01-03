85136f322a77a0914468f564da1d5eaa7d501b09
Sahil <Sahil2003@gmail.com>
2024-01-03 23:33:27 +0530

    Name changed

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,22 +4,27 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
+    title: "ImagineAI",
     description: "Discover & Share AI Prompts",
 };
 
 const RootLayout = ({ children }) => (
     <html lang='en'>
+        <head>
+            <link rel="icon" href="http://localhost:3000/_next/image?url=%2Fassets%2Fimages%2FIA.png&w=64&q=75" />
+        </head>
         <body>
-
-            <div className='main'>
+            <Provider>
                 <div className='gradient' />
-            </div>
+                <div className='main'>
+                    <div className='gradient' />
+                </div>
 
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </Provider>
 
         </body>
     </html>
