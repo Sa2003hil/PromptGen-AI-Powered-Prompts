8446433f02905f62ffcd8ad2aa67ef2cc35833ea
Sahil <Sahil2003@gmail.com>
2024-01-26 19:36:34 +0530

    Feat : Copy text feature added !

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,8 +1,16 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+
+    const [copied, setcopied] = useState("");
+    const handleCopy = () => {
+        setcopied(post.prompt);
+        navigator.clipboard.writeText(post.prompt);
+        setTimeout(() => setcopied(""), 3000);
+    }
+
     return (
         <div className='prompt_card'>
             <div className='flex justify-between  items-start gap-5 '>
@@ -20,9 +28,20 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
                         <p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
                     </div>
                 </div>
+                <div className='copy_btn' onClick={handleCopy}>
+                    <Image
+                        src={copied === post.prompt ? '/assets/icons/tick.svg' :
+                            '/assets/icons/copy.svg'
+                        }
+                        width={12}
+                        height={12}
+                    >
+
+                    </Image>
+                </div>
             </div>
             <p className='my-4 font-santoshi text-sm text-gray-700'>{post.prompt}</p>
-            <p className='font-inter text-sm blue_gradient cursor-pointer'>{post.tag}</p>
+            <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={() => handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
         </div>
     )
 }
